7a9508abbfd8172ec230eddfc47d779787d1c422
Eldwick <jamesjalandoni1@gmail.com>
2014-11-08 15:29:42 -0500

    finishing up gallery

--- app/assets/javascripts/mains.js
+++ app/assets/javascripts/mains.js
@@ -2,6 +2,9 @@ var ready = function () {
   $("#header").hide()
   $('.active').text()
   $('#fullpage').fullpage();
+  $(".galleryImage").hover(function() {
+    $(this).animate({ height: "auto", width: "350px", left: "-=55", top: "-=55" }, "fast");
+  })
   $('#fullpage').on("mousewheel", function () {
     if($('.active h1').text() == "Splash"){
       $("#header").fadeOut(600)
